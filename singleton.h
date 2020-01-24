7a3fe65e57d5264d70054740862dcd6fc3308eb7
asdfgsfe <13126880600@163.com>
2020-01-24 12:52:48 +0800

    fix

--- singleton.h
+++ singleton.h
@@ -47,6 +47,7 @@ private:
 	static void Destory()
 	{
 		typedef char T_must_be_compelete_type[sizeof(T) == 0 ? -1 : 1];
+        T_must_be_compelete_type dumpy; (void)dumpy;
 		delete instance_;
 		instance_ = nullptr;
 	}
